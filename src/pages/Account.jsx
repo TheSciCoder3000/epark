ef5fb29c4b26e1d84ce3e63ee8ae9eca1b2ca4f8
TheSciCoder3000 <drjjdevilla2002@gmail.com>
2025-02-15 11:39:15 +0800

    logout

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,11 +1,19 @@
-import React from "react";
 import Nav from "../components/Nav";
+import { useAuth } from "../components/contexts/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 export default function Account() {
+  const { logOutUser } = useAuth();
+  const navigate = useNavigate();
+
+  const logoutHandler = () => {
+    logOutUser().then(() => navigate("/login"))
+  }
   return (
     <div className="page">
       <h1>👤 Account Section</h1>
       <p>Manage your profile and settings.</p>
+      <button onClick={logoutHandler}>Log Out</button>
       <Nav />
     </div>
   );
