9c586e5b6ca810e0f2d8752540f64a74077b0036
TheSciCoder3000 <drjjdevilla2002@gmail.com>
2025-03-01 22:52:06 +0800

    protected route fix and global nav bar

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,11 +1,15 @@
 import { Navigate, Outlet } from 'react-router'
 import { useAuth } from './contexts/useAuth'
+import Nav from './Nav';
 
 function ProtectedRoute() {
     const { currentUser, loading } = useAuth();
 
     if (!loading) {
-        if (currentUser) return <Outlet />
+        if (currentUser) return <>
+            <Outlet />
+            <Nav />
+        </>
         return <Navigate to="/login" />
     }
 }
