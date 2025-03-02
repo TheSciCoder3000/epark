4eeee58969a72cc1035ca758d6618669c6c85c2a
TheSciCoder3000 <drjjdevilla2002@gmail.com>
2025-03-02 21:28:54 +0800

    vehicle selection component on dashboard

---
+++ src/components/VehicleSelection.jsx
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react"
+
+const VehicleTypes = [
+  {
+    name: "Motorcycle"
+  },
+  {
+    name: "Car"
+  },
+  {
+    name: "Truck"
+  },
+]
+
+function VehicleSelection({ onSelect }) {
+  const [vehicleSelected, setVehicleSelected] = useState(null);
+
+  useEffect(() => onSelect && onSelect(vehicleSelected), [vehicleSelected])
+
+  return (
+    <div className="vehicle-type-cont">
+      {VehicleTypes.map((type, index) => (
+        <div className={`vehicle-type${vehicleSelected === type.name ? " selected" : ""}`}
+          key={index}
+          onClick={() => setVehicleSelected(type.name)}>
+          <img src="" alt={`${type.name}-img`} />
+          <h4>{type.name}</h4>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default VehicleSelection
