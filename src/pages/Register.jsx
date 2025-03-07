8e073280536e5c535a7ebfa901947a6565a24c49
ThermalViz <joshiiichang@gmail.com>
2025-03-08 01:09:50 +0800

    register with vehicle info

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -3,48 +3,88 @@ import { useAuth } from "../components/contexts/useAuth";
 import { useNavigate } from "react-router";
 
 function Register() {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [confirmPassword, setConfirmPassword] = useState("");
-    const { registerUser } = useAuth();
-    const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [vehicleModel, setVehicleModel] = useState("");
+  const [plateNumber, setPlateNumber] = useState("");
+  const { registerUser } = useAuth();
+  const navigate = useNavigate();
 
-    const submitHandler = (e) => {
-        e.preventDefault();
+  const submitHandler = (e) => {
+    e.preventDefault();
 
-        if (confirmPassword == password) {
-            registerUser(email, password).then(() => navigate("/login"));
-        }
+    if (confirmPassword == password) {
+      registerUser(email, vehicleModel, plateNumber, password).then(() => navigate("/login"));
     }
+  };
 
-    return (
-        <div className="register-cont login-cont">
-            <div className="header-cont">
-                <h1>Register</h1>
+  return (
+    <div className="register-cont login-cont">
+      <div className="header-cont">
+        <h1>Register</h1>
+      </div>
+
+      <div className="form-cont">
+        <form onSubmit={submitHandler} className="form-main">
+          <div className="form-field email-field">
+            <label htmlFor="email">Email</label>
+            <input
+              type="text"
+              onChange={(e) => setEmail(e.target.value)}
+              id="email"
+              className="email"
+            />
+          </div>
+          <div className="form-field password-field">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              onChange={(e) => setPassword(e.target.value)}
+              id="password"
+              className="password"
+            />
+          </div>
+          <div className="form-field confirm-password-field">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              id="password"
+              className="password"
+            />
+          </div>
+          <div className="vehicle-container">
+            <h2>Vehicle Info</h2>
+            <div className="form-field vehicle-field">
+              <label htmlFor="vehicle">Vehicle Model</label>
+              <input
+                type="text"
+                onChange={(e) => setVehicleModel(e.target.value)}
+                id="vehicle"
+                className="vehicle"
+              />
             </div>
-
-            <div className="form-cont">
-                <form onSubmit={submitHandler} className="form-main">
-                    <div className="form-field email-field">
-                        <label htmlFor="email">Email</label>
-                        <input type="text" onChange={e => setEmail(e.target.value)} id="email" className="email" />
-                    </div>
-                    <div className="form-field password-field">
-                        <label htmlFor="password">Password</label>
-                        <input type="password" onChange={e => setPassword(e.target.value)} id="password" className="password" />
-                    </div>
-                    <div className="form-field confirm-password-field">
-                        <label htmlFor="password">Password</label>
-                        <input type="password" onChange={e => setConfirmPassword(e.target.value)} id="password" className="password" />
-                    </div>
-
-                    <div className="form-actions">
-                        <button className="register-btn" type="submit">LOGIN</button>
-                    </div>
-                </form>
+            <div className="form-field Plate-field">
+              <label htmlFor="Plate">Plate Number</label>
+              <input
+                type="text"
+                onChange={(e) => setPlateNumber(e.target.value)}
+                id="plate"
+                className="plate"
+              />
             </div>
-        </div>
-    )
+          </div>
+
+          <div className="form-actions">
+            <button className="register-btn" type="submit">
+              REGISTER
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
-export default Register
+export default Register;
