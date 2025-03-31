6d38a380699d8b9e376842a77e7389ecd48dbefe
TheSciCoder3000 <drjjdevilla2002@gmail.com>
2025-03-31 21:41:56 +0800

    fixed style css

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
+import 'rsuite/dist/rsuite.min.css';
 import { BrowserRouter } from 'react-router'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import 'rsuite/dist/rsuite.min.css';
 import { CustomProvider } from 'rsuite'
 
 createRoot(document.getElementById('root')).render(
