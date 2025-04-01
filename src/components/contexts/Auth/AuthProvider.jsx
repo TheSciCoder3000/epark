9aa057c4c58c65a9583ae218490c15488117e25c
TheSciCoder3000 <drjjdevilla2002@gmail.com>
2025-04-01 20:06:34 +0800

    relocated auth contexts

---
+++ src/components/contexts/Auth/AuthProvider.jsx
@@ -0,0 +1,45 @@
+import { onAuthStateChanged } from "firebase/auth";
+import { useEffect, useState } from "react";
+import { auth } from "../../../api/firebase";
+import { loginUser, logOutUser, registerUser } from "../../../api/auth";
+import { AuthContext } from "./useAuth";
+import { getOwnerDb, getUserDb } from "../../../api/Firestore";
+
+export function AuthProvider({ children }) {
+    const [currentUser, setCurrentUser] = useState(null);
+    const [loggedIn, setLoggedIn] = useState(false);
+    const [loading, setLoading] = useState(true);
+
+    const initializeUser = async (user) => {
+        setLoading(true);
+        if (user) {
+            const userData = await getUserDb(user.uid)
+                .then((data) => data ? data : getOwnerDb(user.uid));
+
+            setCurrentUser({ ...userData, uid: user.uid });
+            setLoggedIn(true);
+        } else {
+            setCurrentUser(null);
+            setLoggedIn(false);
+        }
+        setLoading(false);
+    }
+
+    useEffect(() => {
+        const unsub = onAuthStateChanged(auth, initializeUser);
+        return unsub;
+    }, [])
+
+    return (
+        <AuthContext.Provider value={{
+            currentUser,
+            loggedIn,
+            loading,
+            registerUser,
+            loginUser,
+            logOutUser,
+        }}>
+            {children}
+        </AuthContext.Provider>
+    );
+}
