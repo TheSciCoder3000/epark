7c7e104aeb4abdffe91cc577be17c0f311509691
TheSciCoder3000 <drjjdevilla2002@gmail.com>
2025-05-01 22:11:08 +0800

    reservation duration style updates

--- src/components/ParkingTimeSlot.jsx
+++ src/components/ParkingTimeSlot.jsx
@@ -5,12 +5,12 @@ function ParkingTimeSlot({ onStartChange, onEndChange }) {
         <div className='parking-timeslot-cont'>
             <div className="field">
                 <label>Time Start: </label>
-                <TimePicker defaultValue={new Date()} format="hh:mm aa" onChange={onStartChange} showMeridiem />
+                <TimePicker defaultValue={new Date()} format="MM/dd/yyyy hh:mm aa" onChange={onStartChange} showMeridiem />
             </div>
 
             <div className="field">
                 <label>Time End: </label>
-                <TimePicker defaultValue={new Date()} format="hh:mm aa" onChange={onEndChange} showMeridiem />
+                <TimePicker defaultValue={new Date()} format="MM/dd/yyyy hh:mm aa" onChange={onEndChange} showMeridiem />
             </div>
         </div>
     )
