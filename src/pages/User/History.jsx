0369d24be743bd4e2376bb05602d7a2fddb86ffb
TheSciCoder3000 <drjjdevilla2002@gmail.com>
2025-05-01 22:02:34 +0800

    integrating top bar and cleaning ui

--- src/pages/User/History.jsx
+++ src/pages/User/History.jsx
@@ -27,13 +27,7 @@ function History() {
 
     return (
         <div className="history-cont">
-            <div className="top-bar">
-                <button className="back" onClick={onBackClickHandler}>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z" /></svg>
-                </button>
-                <h1>History</h1>
-                <button className="search"></button>
-            </div>
+            <h1>History</h1>
 
             <div className="current-transaction-cont">
 
